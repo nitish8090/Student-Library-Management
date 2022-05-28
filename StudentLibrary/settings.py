77350c24edb5631b9ad691132d503b883e5e160d
nitish <nitishpatel1234@gmail.com>
2022-05-28 21:14:31 +0530

    Added CORS Allow

--- StudentLibrary/settings.py
+++ StudentLibrary/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +131,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "UserManager.User"
+
+CORS_ALLOWED_ORIGINS = [
+    "https://www.nitishpatel.in"
+]
